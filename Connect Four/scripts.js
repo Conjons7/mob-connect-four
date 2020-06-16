67a1d1cdb1be1eeb7ea38c3eb10bd086c16c44ea
Conor <63920243+Conjons7@users.noreply.github.com>
2020-06-16 14:16:06 -0700

    Add files via upload
    beginning of session 6/16/2020

---
+++ Connect Four/scripts.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let player = 1;
+    let grid = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
+    ];
+    function selectColumn(col){
+        if(player == 1) {
+            grid[5][col] = 1;
+            player = 2;
+        } else {
+            grid[5][col] = 2;
+            player = 1;
+        }
+        refreshGrid();
+    }
+    function refreshGrid() {
+        for(let row = 0; row < 6; row++) {
+            for(let col = 0; col < 7; col++) {
+                if(grid[row][col] == 0) {
+                    document.getElementById("cell"+row+col).style.backgroundColor="#FFFFFF";
+                } else if(grid[row][col] == 1) {
+                    document.getElementById("cell"+row+col).style.backgroundColor="#FFFF00";
+                }
+            }
+        }
+    }
+});
